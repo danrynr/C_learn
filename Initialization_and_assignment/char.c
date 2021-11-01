964800ab31b3addca63c143758d9c4f62023f283
danryn <daniel.reynard.k@gmail.com>
2021-11-01 19:59:35 +0700

    Monday, 1 Nov 21

---
+++ Initialization_and_assignment/char.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char c = 65;
+    char c1;
+
+    printf("Karakter = %c\n", c);
+    c1 = 'Z';
+}
---
+++ constant/const.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define FALSE 0
+#define NOL 0
+#define SATU 1
+#define pi 3.1415
+
+int main() {
+    const int maks = 3;
+    const float param = 2.5;
+    const char cc = 65;
+    const char cA = 'A';
+    const float fin = param + maks;
+
+    printf("PI = %6.2f\n", pi);
+    printf("NOL = %d\n", NOL);
+    printf("SATU = %d\n", SATU);
+    printf("FALSE = %d\n", FALSE);
+    printf("maks = %d\n", maks);
+    printf("param = %f\n", param);
+    printf("cc = %c\n", cc);
+    printf("cA = %c\n", cA);
+    printf("3\n");
+
+    printf("fin = %2.2f", fin);
+
+    return 0;
+}
