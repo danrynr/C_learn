4aa4a344156f2343dc3edf387d8f87fced89e511
danryn <daniel.reynard.k@gmail.com>
2021-11-03 21:36:00 +0700

    read write

---
+++ read_write/rw.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+    int a;
+    float x;
+
+    printf("Contoh membaca dan menulis, ketik nilai interger: ");
+    scanf("%d", &a);
+    printf("Nilai yang dibaca : %d\n", a);
+
+    printf("Ketik nilai suatu bilangan rill: ");
+    scanf("%f", &x);
+    printf("Nilai yang dibaca : %f\n", x);
+
+    return 0;
+}
---
+++ read_write/rw2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char cc;
+    char c;
+
+    printf("Hello\n");
+    printf("baca 1 karakter saja : ");scanf("%c ", &cc);
+    printf("%c\n", cc);
+    printf("baca 1 karakter saja : ");scanf("%d ", &c);
+    printf("%d\n", c);
+
+
+    return 0;
+}
