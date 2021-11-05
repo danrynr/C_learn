a72b2c5d1a172ae2137643e50723016e9a971580
danryn <daniel.reynard.k@gmail.com>
2021-11-05 18:10:36 +0700

    Adding 2 more files:
    1. add_int.c
    2. bool_op.c

---
+++ operation/add_int.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    
+    i = 3;
+    printf("Nilai i :%d %d\n", i, i++);
+
+    i = 3;
+    printf("%d\n", ++i);
+
+    i = 3;
+    printf("Nilai i :%d %d\n", i, i--);
+
+    i = 3;
+    printf("%d\n", --i);
+}
