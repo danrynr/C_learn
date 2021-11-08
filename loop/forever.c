3e5d69b18b193d54caf4a07bdc679ebf74508fc5
danryn <daniel.reynard.k@gmail.com>
2021-11-08 19:00:02 +0700

    Adding one folder *loop* :
    Contains for, while, do while loop

---
+++ loop/forever.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Program akan looping, akhiri dengna Ctrl+c ");
+    while (1) {
+        printf("Tunggu ^c....\n");
+    }
+    return 0;
+}
---
+++ loop/while.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+    while (i < 3) {
+        printf("Saya senang belajar bahasa C \n");
+        i++;
+    }
+
+    return 0;
+}
